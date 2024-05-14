d4b8bce11c8cc116c96b658c00bb273d80d4d2ea
Hà Minh Quân <97644076+whitedolpin@users.noreply.github.com>
2024-05-14 09:44:47 +0700

    commit

--- Moda.BackEnd.Domain/Data/ModaDbContext.cs
+++ Moda.BackEnd.Domain/Data/ModaDbContext.cs
@@ -37,8 +37,7 @@ namespace Moda.BackEnd.Domain.Data
         public DbSet<StockHistory> StockHistories { get; set; }
         public DbSet<Tag> Tags { get; set; }
         public DbSet<Warehouse> Warehouses { get; set; }
-        public DbSet<Cart> Carts { get; set; }  
-        public DbSet<CartDetail> CartDetails { get; set; }  
+
         protected override void OnModelCreating(ModelBuilder builder)
         {
             builder.Entity<IdentityRole>().HasData(new IdentityRole
